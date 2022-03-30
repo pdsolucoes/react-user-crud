683f5d928a5af3a1b4b76509eb56e653cea7da5b
Miguel Braga <mbraga@wefit.com.br>
2022-03-30 16:10:05 -0300

    feat: export reset password and confirmation page.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import Login  from "./pages/Login"
 import ForgotPassword from "./pages/ForgotPassword"
+import ResetPassword from "./pages/ResetPassword"
+import Confirmation from "./pages/Confirmation"
 
-export { Login, ForgotPassword }
+export { Login, ForgotPassword, ResetPassword, Confirmation }
